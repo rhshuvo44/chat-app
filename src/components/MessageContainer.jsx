01092309b7a6e5aa5963f8b7674aa5e962da5975
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-02-05 13:14:51 +0600

    no chat seleced component

--- src/components/MessageContainer.jsx
+++ src/components/MessageContainer.jsx
@@ -1,17 +1,23 @@
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
+import NoChatSelected from "./NoChatSelected";
 
 const MessageContainer = () => {
+  const noChatSelected = true;
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      <>
-        <div className="bg-slate-500 px-4 py-2 mb-2">
-          <span className="label-text">To: </span>
-          <span className="text-gray-900 font-bold">John doe</span>
-        </div>
-        <Messages />
-        <MessageInput />
-      </>
+      {noChatSelected ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text">To: </span>
+            <span className="text-gray-900 font-bold">John doe</span>
+          </div>
+          <Messages />
+          <MessageInput />
+        </>
+      )}
     </div>
   );
 };
