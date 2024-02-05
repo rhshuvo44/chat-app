6cdf07deb1b7394f3b932aa4225fd12a158c1e92
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-02-05 11:12:05 +0600

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,42 @@
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Login
+          <span className="text-orange-500"> ChatApp</span>
+        </h1>
+        <form action="">
+          <div>
+            <label htmlFor="" className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="input input-bordered w-full h-10"
+            />
+          </div>
+          <div>
+            <label htmlFor="" className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="input input-bordered w-full h-10"
+            />
+          </div>
+          <a className="text-sm hover:underline hover:text-orange-600" href="#">
+            {"Don't"} have an account
+          </a>
+          <div>
+            <button className="btn btn-block btn-sm mt-2">Login</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
