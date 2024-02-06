059de7da8227408d7a0b1dd6dca7b52ae5c7e6d3
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-02-06 12:13:48 +0600

    log out

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -2,37 +2,22 @@ import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useAuthContext } from "../context/AuthContext";
 
-const useSignup = () => {
+const useLogin = () => {
   const [loading, setLoading] = useState();
   const { setAuthUser } = useAuthContext();
-  const signup = async ({
-    fullName,
-    username,
-    password,
-    confirmPassword,
-    gender,
-  }) => {
-    const success = handleInputErrors({
-      fullName,
-      username,
-      password,
-      confirmPassword,
-      gender,
-    });
+  const login = async ( username, password ) => {
+    const success = handleInputErrors( username, password );
     if (!success) return;
     setLoading(true);
     try {
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch("/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          fullName,
           username,
           password,
-          confirmPassword,
-          gender,
         }),
       });
       const data = await res.json();
@@ -47,29 +32,16 @@ const useSignup = () => {
       setLoading(false);
     }
   };
-  return { loading, signup };
+  return { loading, login };
 };
 
-export default useSignup;
+export default useLogin;
 
-const handleInputErrors = ({
-  fullName,
-  username,
-  password,
-  confirmPassword,
-  gender,
-}) => {
-  if (!fullName || !username || !password || !confirmPassword || !gender) {
+const handleInputErrors = ( username, password ) => {
+  if (!username || !password) {
     toast.error("Please fill in all fields");
     return false;
   }
-  if (password !== confirmPassword) {
-    toast.error("Password do not match");
-    return false;
-  }
-  if (password.length < 6) {
-    toast.error("Password must be at least 6 characters");
-    return false;
-  }
+
   return true;
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../hooks/useLogin";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { loading, login } = useLogin();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(username, password);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -8,7 +17,7 @@ const Login = () => {
           Login
           <span className="text-sky-400"> ChatApp</span>
         </h1>
-        <form action="">
+        <form action="" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="" className="label p-2">
               <span className="text-base label-text">Username</span>
@@ -17,6 +26,8 @@ const Login = () => {
               type="text"
               placeholder="Enter username"
               className="input input-bordered w-full h-10"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div>
@@ -27,6 +38,8 @@ const Login = () => {
               type="password"
               placeholder="Enter password"
               className="input input-bordered w-full h-10"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <Link
@@ -36,7 +49,13 @@ const Login = () => {
             {"Don't"} have an account
           </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2">Login</button>
+            <button disabled={loading} className="btn btn-block btn-sm mt-2">
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Login"
+              )}
+            </button>
           </div>
         </form>
       </div>
